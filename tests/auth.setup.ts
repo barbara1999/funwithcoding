59ce4a1b9c28275f755d490b630790e1e2698530
barbara1999 <1999barbaraa@gmail.com>
2025-03-10 11:17:40 +0100

    initial commit

---
+++ tests/auth.setup.ts
@@ -0,0 +1,30 @@
+import  { test as setup} from '@playwright/test';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import LoginPage from '../page-objects/LoginPage';
+import path from 'path';
+
+const authFile= path.join(__dirname,'../playwright/.auth/user.json');
+
+setup('authenticate',async ({page})=>{
+
+const username= process.env.VALID_USERNAME;
+const password = process.env.VALID_PASSWORD;
+
+    //given
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+
+    //when
+    await loginPage.Login(username,password);
+
+    //then
+    await loginPage.assertValidLogin(page);
+
+    //Save the storage state
+    await page.context().storageState({ path: authFile });
+});
+
