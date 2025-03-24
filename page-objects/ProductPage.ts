c5f0c0b2b45e115d5aace37341d6982800e7f131
barbara1999 <1999barbaraa@gmail.com>
2025-03-24 12:07:45 +0100

    adjusting spacing

--- page-objects/ProductPage.ts
+++ page-objects/ProductPage.ts
@@ -1,54 +1,53 @@
 import { expect, Locator, Page } from "@playwright/test";
 
 
-class ProductPage{
+class ProductPage {
     firstItem: Locator;
     cartBadge: Locator
     itemName: Locator;
     addToChartButton: Locator;
     backToProductsButton: Locator;
-
     readonly page: Page;
 
-    constructor(page:Page){
+    constructor(page: Page) {
         this.page = page
-        this.firstItem= page.locator('#item_4_img_link');
+        this.firstItem = page.locator('#item_4_img_link');
         this.cartBadge = page.locator('[data-test="shopping-cart-badge"]');
         this.itemName = page.locator('[data-test="inventory-item-name"]');
-        this.backToProductsButton= page.locator('#back-to-products');
+        this.backToProductsButton = page.locator('#back-to-products');
         this.addToChartButton = page.locator('#add-to-cart-sauce-labs-backpack');
     }
 
-    async navigate(){
+    async navigate() {
         await this.page.goto('https://www.saucedemo.com/inventory.html');
     }
-    
-    public async clickOnItem(){
+
+    public async clickOnItem() {
         await expect(this.firstItem).toBeVisible();
         await this.firstItem.click();
     }
 
-    public async clickOnBackToProductsButton(){
+    public async clickOnBackToProductsButton() {
         await expect(this.backToProductsButton).toBeVisible();
         await this.backToProductsButton.click();
     }
 
-    public async clickOnAddToChartButton(){
+    public async clickOnAddToChartButton() {
         await expect(this.addToChartButton).toBeVisible();
         await this.addToChartButton.click();
     }
 
-    public async assertItemNameVisible(){
+    public async assertItemNameVisible() {
         await expect(this.itemName).toBeVisible();
     }
 
-    public async assertUrlInventory(){
+    public async assertUrlInventory() {
         await this.page.goto('https://www.saucedemo.com/inventory.html');
         await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html');
 
     }
 
-    public async assertCartBadgeCount(expectedCound: string){
+    public async assertCartBadgeCount(expectedCound: string) {
         await expect(this.cartBadge).toBeVisible();
         await expect(this.cartBadge).toHaveText(expectedCound);
     }
